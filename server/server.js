60bb52897c70fa480e272b928ff42e504a72848b
TikvaYosef <tikvayosef13@gmail.com>
2022-04-12 03:30:27 +0300

    jnkj

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
 require('./db');
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 9090;
-const path = require('path');
-const cors = require('cors');
-app.use(cors());
-app.use(express.json());
 
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 9090;
+const app = express();
 const guestRouter = require('./routes/guest-route');
+app.use(express.json());
+app.use(cors());
+app.listen(PORT, () => console.log("server up"))
+
+
 app.use("/api/wedding",guestRouter);
 
-
-app.listen(PORT, () => console.log("server up"))
- 
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
