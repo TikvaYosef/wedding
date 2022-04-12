ef5002ad46d758d59209374998903936a77d2a2a
TikvaYosef <tikvayosef13@gmail.com>
2022-04-12 10:17:04 +0300

    hk

--- client/src/index.js
+++ client/src/index.js
@@ -5,19 +5,20 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
-// ReactDOM.render(
-//   <React.Fragment>
-//     <App />
-//   </React.Fragment>,
-//   document.getElementById('root')
-// );
 
 
 // If you want your app to work offline and load faster, you can change
